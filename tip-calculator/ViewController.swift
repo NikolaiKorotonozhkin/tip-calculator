ba7a5671b482f801e14e937139a2f2633e810600
NikolaiKorotonozhkin <anikola1k@gmail.com>
2023-12-04 21:39:47 +0300

    Initial Commit

---
+++ tip-calculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  tip-calculator
+//
+//  Created by Nikolai  on 04.12.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+
+
+}
+
